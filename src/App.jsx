c7c69bfab7092e228f0d1744055417d6e159f7f2
Abhishek Vats <virus@Abhisheks-MacBook-Air.local>
2023-03-17 23:15:54 +0530

    version-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import "./App.css";
 import Signin from "./pages/Signin";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Success from "./pages/Success";
 
 function App() {
   return (
     <div className="App">
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/success" element={<Success />} />
           <Route path="/" element={<Signin />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
